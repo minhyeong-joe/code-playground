ec14ae5973d27cdfe767c67a89ba155234223053
minhyeong-joe <minhyeong.billy.joe@gmail.com>
2018-01-10 20:02:16 -0800

    all editors functional. js editor needs improvement.

--- script.js
+++ script.js
@@ -19,8 +19,22 @@ $(function(){
     }
   });
 
-  // html editor
 
+  $('textarea').on('keyup', function() {
+    var target = $('#output')[0].contentWindow.document;
+    target.open();
+    target.close();
+
+    var html = $('#html-editor').val();
+    $('body',target).append(html);
+
+    var css = $('#css-editor').val();
+    $('head',target).append('<style>'+css+'</style>');
+
+    var js = $('#js-editor').val();
+    $('head', target).append('<script type="text/javascript">'+js+'</script>');
+    $( '#output' ).attr( 'src', function ( i, val ) { return val; });
+  });
 
 
 });
